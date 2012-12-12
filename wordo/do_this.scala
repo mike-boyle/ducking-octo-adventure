60a62c0b42ac9c7867de2e4578a29ade213746f5
Mike Boyle <mboyle@backstopsolutions.com>
2012-12-12 15:53:41 -0600

    Add isOffByOne method

--- wordo/do_this.scala
+++ wordo/do_this.scala
@@ -1,5 +1,8 @@
 val start = "dad"
 val end = "zip"
+var current = start
 val lines = io.Source.fromFile("wlist_match1.txt").getLines.filter(_.length == start.length).toIterable
-print(lines.head)
 
+val checked = new collection.immutable.HashSet[String]
+
+def isOffByOne(word: String, other: String): Boolean = (word, other).zipped.map(_ == _).count(!_) == 1
