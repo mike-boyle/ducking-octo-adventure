5721258859defe298064163b22f8497dcb6a2c1e
Mike Boyle <mboyle@backstopsolutions.com>
2012-12-13 12:24:11 -0600

    Use better word list

--- wordo/do_this.scala
+++ wordo/do_this.scala
@@ -18,7 +18,7 @@ time({
 })
 
 def doEverything(start: String, end: String): String = {
-  var lines = io.Source.fromFile("wlist_match1.txt").getLines.filter(_.length == start.length).toIterable.toSet.par
+  var lines = io.Source.fromFile("/usr/share/dict/words").getLines.filter(_.length == start.length).toIterable.toSet.par
 
   val pair = lines.partition(isOffByOne(start, _))
   var offByOne = pair._1.map(x => (x, start + " > " + x)).toMap
