5bfba41d16d26bee2a76ace12482b945fbe4f981
Mike Boyle <mboyle@backstopsolutions.com>
2012-12-13 12:14:09 -0600

    Improve EVERYTHING

--- wordo/do_this.scala
+++ wordo/do_this.scala
@@ -1,37 +1,42 @@
-val start = "flat"
-val end = "dope"
-var lines = io.Source.fromFile("wlist_match1.txt").getLines.filter(_.length == start.length).toIterable.toSet
+val start = "catch"
+val end = "blast"
 
-val pair = lines.partition(isOffByOne(start, _))
-var offByOne = pair._1.map(x => (x, new collection.mutable.Stack() + x)).toMap
-lines = pair._2.toSet
-var distance = 1;
+time({
+  println(start + " > " + end)
+  val answer = doEverything(start, end)
+  println(answer)
+})
 
-while(lines.size > 0 && offByOne.size > 0) { 
-  if (offByOne.contains(end)) {
-    println(distance);
-    println(offByOne.get(end).reduce(_ + " m>m " + _))
-    sys.exit
+def doEverything(start: String, end: String): String = {
+  var lines = io.Source.fromFile("wlist_match1.txt").getLines.filter(_.length == start.length).toIterable.toSet.par
+
+  val pair = lines.partition(isOffByOne(start, _))
+  var offByOne = pair._1.map(x => (x, start + " > " + x)).toMap
+  lines = pair._2.toSet
+
+  while(lines.size > 0 && offByOne.size > 0) { 
+    if (offByOne.contains(end)) {
+      return offByOne.get(end).get
+    }
+
+    val possibilities = offByOne.par.map(x => {
+        val items = lines.filter(isOffByOne(x._1, _)).toSet
+        lines = lines &~ items
+        items.map(y => (y, x._2 + " > " + y))
+      }).flatten.toMap
+
+    offByOne = possibilities
   }
-  else {
-    distance = distance + 1
-  }
 
-  println("offByOneSize: " + offByOne.size)
-
-  val possibilities = offByOne.map(x => {
-      val items = lines.filter(isOffByOne(x._1, _)).toSet
-      lines = lines &~ items
-      items.map(y => (y, x._2 + y))
-    }).flatten.toMap
-
-  println("possibiliesSize: " + possibilities.size)
-  println("linesSize: " + lines.size)
-  lines = lines &~ possibilities.keys.toSet
-
-  offByOne = possibilities
+  return "FAILURE"
 }
 
-println("FAILURE")
-
 def isOffByOne(word: String, other: String): Boolean = (word, other).zipped.map(_ == _).count(!_) == 1
+
+def time[A](a: => A) = {
+        val now = System.nanoTime
+        val result = a
+        val seconds = (System.nanoTime - now) / 1000000000
+        println("%d seconds".format(seconds))
+        result
+      }
