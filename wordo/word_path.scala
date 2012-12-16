a4cc2a9698d2e8e60a1166d68344a2ca1ca9e72e
Mike Boyle <michael.boyle@gmail.com>
2012-12-16 13:20:28 -0600

    More idiomatic scala maybe

---
+++ wordo/word_path.scala
@@ -0,0 +1,41 @@
+if(args.size != 2) {
+  println("I HATE YOU")
+  sys.exit(1)
+}
+
+val start = args(0)
+val end = args(1)
+
+if(start.length != end.length) {
+  println("IDIOT")
+  sys.exit(1)
+}
+
+var lines = io.Source.fromFile("/usr/share/dict/words").getLines
+  .filter(_.length == start.length).toIterable.toSet
+
+if(!lines.contains(start) || !lines.contains(end)) {
+  println("BAD WORD ALERT")
+  sys.exit(1)
+}
+
+println(start + " > " + end)
+println(getAnswer)
+
+def getAnswer: String = {
+  val pair = lines.partition(isOffByOne(start, _))
+  val offByOne = pair._1.map(x => (x, start + " > " + x)).toMap
+  lines = pair._2.toSet
+  val stream = collection.Iterator.iterate(offByOne)(getNeighbors(_))
+  stream.find(x => x.contains(end) || x.isEmpty).get.getOrElse(end, "FAILURE")
+}
+
+def getNeighbors(words: Map[String, String]): Map[String, String] = {
+  words.map(x => {
+    val items = lines.filter(isOffByOne(x._1, _)).toSet
+    lines = lines &~ items
+    items.map(y => (y, x._2 + " > " + y))
+  }).flatten.toMap
+}
+
+def isOffByOne(word: String, other: String) = (word, other).zipped.map(_ == _).count(!_) == 1
