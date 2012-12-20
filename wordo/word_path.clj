8d4d002a98b9bf4b138787cfad88b40999c0504f
Mike Boyle <mboyle@backstopsolutions.com>
2012-12-19 18:21:08 -0600

    Moved some code around in clojure wordo.
    Still have no clue what I'm doing.

--- wordo/word_path.clj
+++ wordo/word_path.clj
@@ -3,32 +3,30 @@
 
 (def words ["dad" "rat" "rad"])
 
-(defn matcho [a b]
+(defn mitch [a b]
   (and 
    (= 1 
       (count 
       	(filter false? (map = a b)))) 
    (= (count a) (count b))))
 
-(defn neigh [cur words]
-  (mapcat (fn [word] 
-            (filter 
-             (fn [other]
-               (matcho word other))
-             words)) cur))
+(defn nate [carl waldo]
+  (mapcat 
+   #(filter 
+     (partial mitch %)
+     waldo)
+   carl))
 
 (defn finished [set]
   (or 
   	(contains? end set)
-    (empty? set)
-   ))
+    (empty? set)))
           
-(matcho "rad" "dad")
+(mitch "rad" "dad")
            
-(neigh ["rad" "bob"] ["fad" "dad" "bot" "oob"])
+(nate ["bad" "pot"] ["fad" "dad" "bot" "oob"])
 
-(first (filter finished
-               (iterate (fn [[cur words]] 
-                   (neigh [cur] words))
-                   [start words])))
+(take 5 
+      (iterate #(nate [%1] words) 
+               start))
           
